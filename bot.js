a58dfd4b36f188ea5858996075dccb6795a6bc08
Wijayanto Abdurrahman Prawirodirjo <wijayanto.a.p@gmail.com>
2020-08-18 17:46:31 +0700

    refine token

--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
 var TwitterPackage = require("twitter");
-let TwitterBot = require("node-twitterbot").TwitterBot;
 
-let secret = new TwitterBot({
+let secret = {
   consumer_key: process.env.BOT_CONSUMER_KEY,
   consumer_secret: process.env.BOT_CONSUMER_SECRET,
   access_token: process.env.BOT_ACCESS_TOKEN,
   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
-});
+};
 var Twitter = new TwitterPackage(secret);
 
 // Call the stream function and pass in 'statuses/filter', our filter object, and our callback
